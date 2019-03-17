940361818cb37e93d41c81581010e6766be5da7a
Blake Mason <blakelmason@gmail.com>
2019-03-17 12:54:24 -0700

    Fixed server variable.

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ server.use(bodyParser.json());
 server.use(routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", function (req, res) {
+  server.use(express.static("client/build"));
+  server.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
