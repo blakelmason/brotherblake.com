b223f7062db32d68628c4ea7a6559120ca9e5b15
Blake Mason <blakelmason@gmail.com>
2019-03-17 12:23:55 -0700

    Initial setup.

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require('path');
+const routes = require('./routes');
+
+// express setup
+const server = express();
+const PORT = 3001;
+
+// body parser configuration
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json());
+
+//routes
+server.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+  server.use("/", express.static(__dirname + "/client/build"));
+
+  server.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+server.listen(PORT, function () {
+  console.log(`🌎  ==> Server listening on PORT ${PORT}!`);
+});
