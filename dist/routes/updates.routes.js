011ea6afc54a121d0e9d4cc03e022728785a8a0f
Blake Mason <blakelmason@gmail.com>
2019-04-21 13:10:26 -0700

    Forgot babel.

--- dist/routes/updates.routes.js
+++ dist/routes/updates.routes.js
@@ -15,6 +15,22 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var router = _express.default.Router();
 
+router.get('/2019-April-21', function (request, response) {
+  var filePath = _path.default.join(__dirname, '../../client/build/index.html');
+
+  _fs.default.readFile(filePath, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    data = data.replace(/\$OG_TITLE/g, 'Update - April 14, 2019');
+    data = data.replace(/\$OG_DESCRIPTION/g, 'The First Update!');
+    data = data.replace(/\$OG_URL/g, 'http://www.brotherblake.com/updates/2019-April-21');
+    data = data.replace(/\$OG_SITENAME/g, 'BrotherBlake.com');
+    var result = data.replace(/\$OG_IMAGE/g, 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/57774973_2122646601123669_6081006171613822976_o.jpg?_nc_cat=110&_nc_ht=scontent-lax3-2.xx&oh=9850df85dfa4142bd4f5ff28946dd399&oe=5D340213');
+    response.send(result);
+  });
+});
 router.get('/2019-April-14', function (request, response) {
   var filePath = _path.default.join(__dirname, '../../client/build/index.html');
 
