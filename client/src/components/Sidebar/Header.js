00ea762472b3894313f8dedb674df7821454c252
blakelmason <blakelmason@gmail.com>
2019-07-17 11:56:11 -0700

    Reset.

--- client/src/components/Sidebar/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = ({ children }) => {
-  return <div className="border bg-white">{children}</div>;
-};
-
-export default Header;
--- client/src/pages/Updates/list.js
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-
-const list = ['2019-04-21', '2019-04-14'];
-
-list.forEach((date, index) => {
-  list[index] = moment(date);
-});
-
-export default list;
