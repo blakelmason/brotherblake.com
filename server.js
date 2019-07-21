47790df08e24260c4390ecbc320452f3cec71f83
Blake Mason <blakelmason@gmail.com>
2019-07-21 14:13:11 -0700

    Ready for Heroku.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3001
 process.env.NODE_ENV === 'production' &&
   server.use(express.static('client/build'))
 
-app.get('*', (req, res) => {
+server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
